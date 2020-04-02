c49ab73cd7f6251a22be004b0211beee3420fc95
vsGuia <60855850+vsGuia@users.noreply.github.com>
2020-04-02 18:07:34 -0300

    Front-End do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
-
-//css
-import './global.css'
-
-import Routes  from './routes'
-
-
-// JSX (javascript XML)
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
   return (
-    <Routes/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World and Omnistack
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
